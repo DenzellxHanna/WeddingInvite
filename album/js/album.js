3ad6dd5874e39cf1b02ac8f11857ed1e2e1460a8
DenzellxHanna <denzellhannamallari@gmail.com>
2026-09-13 02:17:49 +0800

    feat: add album cover arrival after curtain reveal

--- album/js/album.js
+++ album/js/album.js
@@ -12,6 +12,16 @@ export function createAlbum(root) {
     mount() {
       root.dataset.system = 'album';
       root.dataset.state = 'closed';
+      root.innerHTML = `
+        <div class="album-arrival" aria-label="Wedding album">
+          <div class="album-cover">
+            <div class="album-cover__frame"></div>
+            <div class="album-cover__content">
+              <span class="album-cover__title">D/H WEDDING</span>
+            </div>
+          </div>
+        </div>
+      `;
     },
     receivePhoto(photo) {
       root.dataset.photoId = photo.id;
